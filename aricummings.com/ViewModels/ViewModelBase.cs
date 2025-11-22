1b7eed1e15629275d71bd7beba0a19dfeefd51dd
Ari Cummings <ari@aricummings.com>
2025-11-22 12:07:00 -0800

    Initial commit

---
+++ aricummings.com/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace aricummings.com.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
---
+++ aricummings.com/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace aricummings.com.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
